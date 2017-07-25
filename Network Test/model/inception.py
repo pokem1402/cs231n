1aba3e8d8247232482230768266e488580e15059
wonbin <pokem@naver.com>
2017-07-25 21:24:53 +0900

    add

--- Network Test/model/inception.py
+++ Network Test/model/inception.py
@@ -498,3 +498,113 @@ def ver4(X,y,is_training):
     out = tf.nn.avg_pool(out, [1,16,16,1], [1,1,1,1], padding='VALID')
     y_out = tf.reshape(out, [-1, num_classes])
     return y_out
+def ver5(X,y,is_training):
+    ##############################################################
+    # 3x3 conv layer - > 1x3 conv + 3x1 conv
+    ##############################################################
+    module_iter = 10
+    num_classes = 10
+    MO = 64 # module output/4
+    [N,W,H,C] = X.shape
+    """
+    setup variables
+    """
+    # 1x1 conv
+    W1conv0 = tf.get_variable("W1conv0", shape=[1,1,C,MO])
+    b1conv0 = tf.get_variable("b1conv0",shape=[MO])
+    ##############################################################
+    #
+    # inception module v3
+    #
+    # layer | 1x1 conv + (1x1conv-1x3 conv+ 3x1 conv) + [1x1-conv-1x3 conv+ 3x1 conv - 1x3 conv+ 3x1 conv] + [max pool-1x1 conv]
+    #   C    |      4             4               4               4         = 16
+    ##############################################################
+    # 1x1 conv
+    W1conv1 = [tf.get_variable("W1conv1"+str(i), shape=[1,1,MO,MO/4]) for i in range(1,module_iter+1)]
+    b1conv1 = [tf.get_variable("b1conv1"+str(i), shape=[MO/4] )for i in range(1,module_iter+1)]
+    # 3x3 conv
+    W3conv1= [tf.get_variable("W3conv1"+str(i), shape=[1,1,MO,MO/4]) for i in range(1,module_iter+1)]
+    b3conv1 = [tf.get_variable("b3conv1"+str(i), shape=[MO/4] )for i in range(1,module_iter+1)]
+    W3conv2 = [tf.get_variable("W3conv2"+str(i), shape=[1,3,MO/4,MO/4]) for i in range(1,module_iter+1)]
+    b3conv2 = [tf.get_variable("b3conv2"+str(i), shape=[MO/4]) for i in range(1,module_iter+1)]
+    W3conv3 = [tf.get_variable("W3conv3"+str(i), shape=[3,1,MO/4,MO/4]) for i in range(1,module_iter+1)]
+    b3conv3 = [tf.get_variable("b3conv3"+str(i), shape=[MO/4]) for i in range(1,module_iter+1)]
+    # 5x5 conv
+    W5conv1 = [tf.get_variable("W5conv1"+str(i), shape=[1,1,MO,MO/4]) for i in range(1,module_iter+1)]
+    b5conv1 = [tf.get_variable("b5conv1"+str(i), shape=[MO/4] )for i in range(1,module_iter+1)]
+    W5conv2 = [tf.get_variable("W5conv2"+str(i), shape=[1,3,MO/4,MO/4]) for i in range(1,module_iter+1)]
+    b5conv2 = [tf.get_variable("b5conv2"+str(i), shape=[MO/4]) for i in range(1,module_iter+1)]
+    W5conv3 = [tf.get_variable("W5conv3"+str(i), shape=[3,1,MO/4,MO/4]) for i in range(1,module_iter+1)]
+    b5conv3 = [tf.get_variable("b5conv3"+str(i), shape=[MO/4]) for i in range(1,module_iter+1)]
+    W5conv4 = [tf.get_variable("W5conv4"+str(i), shape=[1,3,MO/4,MO/4]) for i in range(1,module_iter+1)]
+    b5conv4 = [tf.get_variable("b5conv4"+str(i), shape=[MO/4]) for i in range(1,module_iter+1)]
+    W5conv5 = [tf.get_variable("W5conv5"+str(i), shape=[3,1,MO/4,MO/4]) for i in range(1,module_iter+1)]
+    b5conv5 = [tf.get_variable("b5conv5"+str(i), shape=[MO/4]) for i in range(1,module_iter+1)]
+    # max pool
+    W1conv2 = [tf.get_variable("W1conv2"+str(i), shape=[1,1,MO,MO/4]) for i in range(1,module_iter+1)]
+    b1conv2 = [tf.get_variable("b1conv2"+str(i), shape=[MO/4] )for i in range(1,module_iter+1)]
+    ##############################################################
+    #
+    #  1x1 conv
+    #
+    ##############################################################
+    W1conv3 = tf.get_variable("W1conv3", shape=[1,1,MO, MO/4])
+    b1conv3 = tf.get_variable("b1conv3", shape=[MO/4]) # F
+    W1conv4 = tf.get_variable("W2conv3", shape=[1,1,MO/4, num_classes])
+    b1conv4 = tf.get_variable("b2conv3", shape=[num_classes]) # F
+
+    """
+     define graph
+    """
+    out = X
+    ##############################################################
+    #
+    #  1. max pool (2x2) stride = 2
+    #
+    #  To reduce window size
+    #
+    #  INPUT : [N,W,H,C]
+    # OUTPUT : [N,W/2,H/2,C]
+    ##############################################################
+    out = tf.nn.max_pool(out, [1,2,2,1], [1,2,2,1], padding='VALID')
+    out = tf.nn.conv2d(out, W1conv0, strides=[1,1,1,1], padding='VALID', name='init')+b1conv0
+    ##############################################################
+    #
+    #  2. inception_moduel
+    #
+    #  Concatenate result of 1x1 conv, 3x3conv, 5x5 conv, max pool
+    #
+    #  INPUT : [N,W/2,H/2,C]
+    #  OUTPUT : [N,W/2, H/2, C]
+    #
+    ##############################################################
+    for i in range(module_iter):
+        # 1x1 conv
+        out1 = tf.nn.conv2d(out, W1conv1[i], strides = [1,1,1,1], padding='VALID',name="one")+b1conv1[i]
+        # 3x3 conv
+        out3 = tf.nn.conv2d(out,  W3conv1[i], strides = [1,1,1,1], padding='SAME',name="three1")+b3conv1[i]
+        out3 = tf.nn.conv2d(out3, W3conv2[i], strides = [1,1,1,1], padding='SAME',name="three2")+b3conv2[i]
+        out3 = tf.nn.conv2d(out3, W3conv3[i], strides = [1,1,1,1], padding='SAME',name="three3")+b3conv3[i]
+        # 5x5 conv
+        out5 = tf.nn.conv2d(out,  W5conv1[i], strides = [1,1,1,1], padding='SAME', name="five1")+b5conv1[i]
+        out5 = tf.nn.conv2d(out5, W5conv2[i], strides = [1,1,1,1], padding='SAME',name="five2")+b5conv2[i]
+        out5 = tf.nn.conv2d(out5, W5conv3[i], strides = [1,1,1,1], padding='SAME',name="five3")+b5conv3[i]
+        out5 = tf.nn.conv2d(out5, W5conv4[i], strides = [1,1,1,1], padding='SAME',name="five4")+b5conv4[i]
+        out5 = tf.nn.conv2d(out5, W5conv5[i], strides = [1,1,1,1], padding='SAME',name="five5")+b5conv5[i]
+        # max
+        out_max = tf.nn.max_pool(out, [1,3,3,1], [1,1,1,1], padding='SAME',name="max1")
+        out_max = tf.nn.conv2d(out_max, W1conv2[i], strides=[1,1,1,1], padding='SAME', name="max2")+b1conv2[i]
+        out = tf.concat([out1, out3, out5, out_max], 3)
+        out = tf.nn.relu(out)
+    ##############################################################
+    #
+    #  3. Fully Connected(Using 1x1 conv)
+    #
+    #  INPUT : [N,W/2,H/2,C]
+    #  OUTPUT : [#Class]
+    ##############################################################
+    out = tf.nn.conv2d(out, W1conv3, strides=[1,1,1,1], name="one_last1", padding='VALID')+b1conv3
+    out = tf.nn.conv2d(out, W1conv4, strides=[1,1,1,1], name="one_last2", padding='VALID')+b1conv4
+    out = tf.nn.avg_pool(out, [1,16,16,1], [1,1,1,1], padding='VALID')
+    y_out = tf.reshape(out, [-1, num_classes])
+    return y_out
---
+++ assignment3/readme.md
@@ -0,0 +1,7 @@
+#To obtain datasets for 
+ 1. RNN captioning
+ 2. LSTM captioning
+
+ """
+ sudo bash /cs231n/datasets/get_assignment3_data.sh
+ """
